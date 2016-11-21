a1a25936873f1110d3fae81e83674591017fbddb
Kevin Carwile <kcarwile@gmail.com>
2016-11-21 00:03:20 -0800

    tabs fix

--- classes/BasicWidget.php
+++ classes/BasicWidget.php
@@ -48,15 +48,15 @@ class BasicWidget extends \Modern\Wordpress\Plugin\Widget
 	 */
 	public $classname = 'boilerplate-widget';
 	
-    /**
-     * Output the widget content.
+	/**
+	 * Output the widget content.
 	 *
-     * @param 	array 	$args     	Display arguments including 'before_title', 'after_title', 'before_widget', and 'after_widget'.
-     * @param 	array 	$instance 	The settings for the particular instance of the widget.
-     */
-    public function widget( $args, $instance ) 
+	 * @param 	array 	$args     	Display arguments including 'before_title', 'after_title', 'before_widget', and 'after_widget'.
+	 * @param 	array 	$instance 	The settings for the particular instance of the widget.
+	 */
+	public function widget( $args, $instance ) 
 	{
-        echo $this->getPlugin()->getTemplateContent( 'widget/layout/standard', array( 'args' => $args, 'widget_title' => $instance[ 'title' ], 'widget_content' => $instance[ 'content' ] ) );
-    }
+		echo $this->getPlugin()->getTemplateContent( 'widget/layout/standard', array( 'args' => $args, 'widget_title' => $instance[ 'title' ], 'widget_content' => $instance[ 'content' ] ) );
+	}
 	
 }

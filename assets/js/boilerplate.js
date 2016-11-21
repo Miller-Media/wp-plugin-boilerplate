2d46f7000760ef4108e52ca582cf860c144e3366
Kevin Carwile <kcarwile@gmail.com>
2016-11-20 23:51:09 -0800

    updated javascript boilerplate example

--- assets/js/boilerplate.js
+++ assets/js/boilerplate.js
@@ -1,19 +1,22 @@
 /**
+ * Javascript Module
+ *
  * @package 	Boilerplate Wordpress Plugin
  * @author 		Kevin Carwile (Miller Media)
  * @since		Nov 2016
  */
 
-use "strict";
+"use strict";
 
 /**
  * Module Design Pattern
  */
 (function( $, undefined ) {
-	var myModule = (function() {
+	
+	var thisModule = new function() {
 		
 		/**
-		 * @var	this
+		 * @var	Module Global "this"
 		 */
 		var $this = this;
 		
@@ -26,31 +29,39 @@ use "strict";
 		$this.local = mw_localized_data;
 		
 		/**
-		 * Module Methods 
+		 * Named module variable
+		 * 
+		 * @var  
+		 */
+		$this.namedVar = null;
+		
+		
+		/**
+		 * Main Module Init (runs after page is loaded)
+		 *
+		 * @return	void
 		 */
 		$.extend( $this, {
-			
-			init: function()
-			{
-				// Kick off page processing...
-				$this.doSomething();
-			},
-			
-			/**
-			 * Example method
-			 *
-			 * @return void
-			 */
-			doSomething()
-			{
-
-			}
+		init: function()
+		{
+			// Now we can do stuff like attach our methods to the DOM
+			$( document ).on( 'sasquatch.spotted', $this.doSomething );
+		},
 		
-		});
-	})();
+		/**
+		 * Example module method
+		 *
+		 * @return void
+		 */
+		doSomething()
+		{
+			console.log( 'Hello Harry!' );
+		}
+		
+	});	};
 	
 	/* Initialize when document is ready */
-	$( document ).ready( myModule.init );
+	$( document ).ready( thisModule.init );
 	
 })( jQuery );
  

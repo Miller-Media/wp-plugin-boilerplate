699aaf6ef78d876fac46b0b301314d02b4edf097
Kevin Carwile <kcarwile@gmail.com>
2017-01-05 08:48:01 -0800

    javascript file renamed

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
 "use strict";
 
 /**
- * Module Design Pattern
+ * Controller Design Pattern
  *
  * Note: This pattern requires your script to have a dependency on the "mwp" script
  * i.e. @Wordpress\Script( deps={"mwp"} )
@@ -19,17 +19,17 @@
 (function( $, undefined ) {
 	
 	/**
-	 * Main Module
+	 * Main Controller
 	 *
 	 * The init() function is called after the page is fully loaded.
 	 *
 	 * Data passed into your script from the server side is available
-	 * by the thisModule.local property inside your module:
+	 * by the mainController.local property inside your controller:
 	 *
-	 * > var ajaxurl = thisModule.local.ajaxurl;
+	 * > var ajaxurl = mainController.local.ajaxurl;
 	 *
-	 * The viewModel of your module will be bound to any HTML structure
-	 * which uses the data-view-model attribute and names this module.
+	 * The viewModel of your controller will be bound to any HTML structure
+	 * which uses the data-view-model attribute and names this controller.
 	 *
 	 * Example:
 	 *
@@ -37,7 +37,7 @@
 	 *   <span data-bind="text: title"></span>
 	 * </div>
 	 */
-	var thisModule = mwp.module( '{plugin_slug}', 
+	var mainController = mwp.controller( '{plugin_slug}', 
 	{
 		
 		/**
@@ -47,11 +47,11 @@
 		 */
 		init: function()
 		{
-			// ajax actions can be made to the ajaxurl, which is automatically provided to your module
-			var ajaxurl = thisModule.local.ajaxurl;
+			// ajax actions can be made to the ajaxurl, which is automatically provided to your controller
+			var ajaxurl = mainController.local.ajaxurl;
 			
 			// set the properties on your view model which can be observed by your html templates
-			thisModule.viewModel = 
+			mainController.viewModel = 
 			{
 				title: ko.observable( '{plugin_name}' )
 			}

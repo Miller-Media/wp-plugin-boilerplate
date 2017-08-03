146964d4e163a2c8f8d721a1151f09b2e90b1f38
Kevin Carwile <kcarwile@gmail.com>
2017-08-03 15:11:30 -0700

    added example dependencies for reference

--- includes/plugin-dependency-config.php
+++ includes/plugin-dependency-config.php
@@ -9,7 +9,28 @@ include_once 'class-tgm-plugin-activation.php';
 add_action( 'tgmpa_register', function() 
 {
 	$base_dir = dirname( dirname( __FILE__ ) );
-	$dependencies = array();
+	$dependencies = array(
+		/* This is an example of how to include a plugin dependency. See: http://tgmpluginactivation.com/configuration/#h-plugin-parameters
+		array(
+			'name'               => 'TGM Example Plugin', // The plugin name.
+			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'BuddyPress',
+			'slug'      => 'buddypress',
+			'required'  => false,
+		),
+		*/
+	);
 	
 	if ( file_exists( $base_dir . '/data/plugin-dependencies.php' ) )
 	{
